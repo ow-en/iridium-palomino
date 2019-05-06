345415bb4b76a2fb8312ca9bfc92b54eda68266e
ow-en <owen.email.sd@gmail.com>
2019-05-06 01:20:26 -0700

    more animations

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,6 +4,77 @@
       <div class="column is-one-third is-multiline">
         <section class="hero is-fluid">
           <div class="hero-body text-hero">
+            <div class="draw">
+              <svg
+                id="cDev"
+                viewBox="0 0 93 57"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="1.414"
+              >
+                <path fill="none" d="M0 -5H492.268V61.31H0z" />
+                <g fill="none" stroke="#000">
+                  <path
+                    class="logo-stroke"
+                    d="M13.208,29.682c0,-11.1 6.51,-18.226 15.28,-18.226c6.784,0 12.06,4.248 13.294,10.826l7.743,0c-1.302,-10.621 -9.799,-17.13 -21.037,-17.13c-13.773,0 -23.023,9.798 -23.023,24.53c0,14.733 9.25,24.531 23.023,24.531c11.238,0 19.735,-6.509 21.037,-17.13l-7.743,0c-1.234,6.578 -6.51,10.826 -13.294,10.826c-8.77,0 -15.28,-7.126 -15.28,-18.227Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M85.91,53.459l7.743,0l-18.707,-47.554l-9.113,0l-18.638,47.554l7.537,0l4.797,-12.196l21.653,0l4.728,12.196Zm-15.555,-41.044l8.634,22.612l-17.268,0l8.634,-22.612Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M112.428,54.213c11.511,0 19.46,-6.235 19.46,-19.323l0,-28.985l-7.332,0l0,28.985c0,8.223 -4.728,12.951 -12.128,12.951c-7.332,0 -12.06,-4.728 -12.06,-12.951l0,-28.985l-7.332,0l0,28.985c0,13.088 7.948,19.323 19.392,19.323Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M136.684,53.459l32.274,0l0,-6.646l-24.942,0l0,-40.908l-7.332,0l0,47.554Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M171.151,53.459l7.331,0l0,-20.693l22.202,0l0,-6.373l-22.202,0l0,-14.047l25.285,0l0,-6.441l-32.616,0l0,47.554Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M205.823 5.905H213.155V53.459H205.823z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M218.842,53.459l34.672,0l0,-6.509l-27.34,0l0,-14.664l24.462,0l0,-6.304l-24.462,0l0,-13.636l26.929,0l0,-6.441l-34.261,0l0,47.554Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M257.145,53.459l32.274,0l0,-6.646l-24.942,0l0,-40.908l-7.332,0l0,47.554Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M332.999,29.682c0,-13.841 -8.36,-23.777 -23.846,-23.777l-17.61,0l0,47.554l17.61,0c15.486,0 23.846,-9.867 23.846,-23.777Zm-7.743,0c0,10.69 -6.098,17.405 -16.308,17.405l-10.073,0l0,-34.809l10.073,0c10.21,0 16.308,6.715 16.308,17.404Z"
+                  />
+                  <path
+                    class="logo-stroke"
+                    d="M333.41 44.757H342.112V53.458999999999996H333.41z"
+                  />
+                  <path
+                    :class="{ 'logo-stroke': true, 'svg-filled': svgFilled }"
+                    d="M388.502,29.682c0,-13.841 -8.36,-23.777 -23.846,-23.777l-17.61,0l0,47.554l17.61,0c15.486,0 23.846,-9.867 23.846,-23.777Zm-7.743,0c0,10.69 -6.099,17.405 -16.308,17.405l-10.073,0l0,-34.809l10.073,0c10.209,0 16.308,6.715 16.308,17.404Z"
+                  />
+                  <path
+                    :class="{ 'logo-stroke': true, 'svg-filled': svgFilled }"
+                    d="M391.517,53.459l34.672,0l0,-6.509l-27.341,0l0,-14.664l24.463,0l0,-6.304l-24.463,0l0,-13.636l26.93,0l0,-6.441l-34.261,0l0,47.554Z"
+                  />
+                  <path
+                    :class="{ 'logo-stroke': true, 'svg-filled': svgFilled }"
+                    d="M433.109,5.905l-7.811,0l17.13,47.554l9.319,0l17.336,-47.554l-7.606,0l-14.321,40.017l-14.047,-40.017Z"
+                  />
+                  <path
+                    :class="{ 'logo-stroke': true, 'svg-filled': svgFilled }"
+                    d="M468.467,15.087c0,5.482 4.522,9.867 10.141,9.867c5.55,0 10.141,-4.385 10.141,-9.867c0,-5.482 -4.591,-9.935 -10.141,-9.935c-5.619,0 -10.141,4.453 -10.141,9.935Zm4.865,0c0,-3.015 2.329,-5.619 5.276,-5.619c2.878,0 5.276,2.604 5.276,5.619c0,3.015 -2.398,5.55 -5.276,5.55c-2.947,0 -5.276,-2.535 -5.276,-5.55Z"
+                  />
+                </g>
+              </svg>
+            </div>
             <p class="text">
               <span class="text__first">
                 <span class="text__word lower-text">
@@ -13,35 +84,35 @@
               </span>
               <br />
               <span class="text__second">
-                <span class="text__word lower-text text-turquoise">
+                <span id="dev" class="text__word lower-text text-turquoise">
                   .dev
                 </span>
                 <span class="text__second-bg"> </span>
               </span>
               <br />
               <span class="text__third">
-                <span class="text__word lower-text">
+                <span class="text__word lower-text text-light">
                   a
                 </span>
                 <span class="text__third-bg"> </span>
               </span>
               <br />
               <span class="text__fourth">
-                <span class="text__word lower-text">
+                <span class="text__word lower-text text-light">
                   blog
                 </span>
                 <span class="text__fourth-bg"> </span>
               </span>
               <br />
               <span class="text__fifth">
-                <span class="text__word lower-text">
+                <span class="text__word lower-text text-light">
                   about
                 </span>
                 <span class="text__fifth-bg"> </span>
               </span>
               <br />
               <span class="text__sixth">
-                <span class="text__word lower-text">
+                <span class="text__word lower-text text-light">
                   programming
                 </span>
                 <span class="text__sixth-bg"> </span>
@@ -52,20 +123,22 @@
       </div>
       <div class="column is-two-thirds"></div>
     </div>
-    <h2 class="title is-3">Recent Posts</h2>
-    <div id="recent-posts" class="columns is-multiline is-centered">
-      <div
-        class="column is-half"
-        v-for="post in $page.recentPosts.edges"
-        :key="post.node.id"
-      >
-        <g-link :to="post.node.path" :aria-label="post.node.title">
-          <g-image
-            :src="post.node.cover.src"
-            class="cover"
-            :alt="post.node.title"
-          />
-        </g-link>
+    <div class="posts-container">
+      <h2 class="title is-3">Recent Posts</h2>
+      <div id="recent-posts" class="columns is-multiline is-centered">
+        <div
+          class="column is-half"
+          v-for="post in $page.recentPosts.edges"
+          :key="post.node.id"
+        >
+          <g-link :to="post.node.path" :aria-label="post.node.title">
+            <g-image
+              :src="post.node.cover.src"
+              class="cover"
+              :alt="post.node.title"
+            />
+          </g-link>
+        </div>
       </div>
     </div>
   </Layout>
@@ -89,6 +162,8 @@ query Posts {
 <script>
 // import Search from "@/components/Search";
 import { TweenMax, TimelineLite } from "gsap";
+import drawSVG from "@/gsap/DrawSVGPlugin";
+import { setTimeout } from "timers";
 
 export default {
   components: {},
@@ -111,6 +186,12 @@ export default {
       { name: "twitter:creator", content: "@therealdanvega" }
     ]
   },
+  data() {
+    return {
+      hidden: false,
+      svgFilled: false
+    };
+  },
   methods: {
     animateText() {
       console.log("animate");
@@ -121,7 +202,9 @@ export default {
         fourthBg = document.querySelectorAll(".text__fourth-bg"),
         fifthBg = document.querySelectorAll(".text__fifth-bg"),
         sixthBg = document.querySelectorAll(".text__sixth-bg"),
-        word = document.querySelectorAll(".text__word");
+        word = document.querySelectorAll(".text__word"),
+        textLight = document.querySelectorAll(".text-light"),
+        posts = document.querySelectorAll(".posts-container");
 
       tl.to(firstBg, 0.2, { scaleX: 1 })
         .to(secBg, 0.2, { scaleX: 1 })
@@ -132,15 +215,27 @@ export default {
         .to(word, 0.1, { opacity: 1 }, "-=0.1")
 
         .to(firstBg, 0.2, { scaleX: 0 })
-        .to(secBg, 0.2, { scaleX: 0 })
+        .to(secBg, 0.2, { scaleX: 1 })
         .to(thirdBg, 0.2, { scaleX: 0 })
         .to(fourthBg, 0.2, { scaleX: 0 })
         .to(fifthBg, 0.2, { scaleX: 0 })
-        .to(sixthBg, 0.2, { scaleX: 0 });
+        .to(sixthBg, 0.2, { scaleX: 0 })
+        .to(textLight, 0.3, { opacity: 0.3 })
+        .to("#dev", 0.2, { rotation: "360_cw" })
+        .to(posts, 1.3, { opacity: 1 });
+    },
+    drawLogo() {
+      console.log("logo");
+      var tl = new TimelineLite({ delay: 1 });
+      tl.from(".logo-stroke", 4, { drawSVG: 0, ease: Power1.easeInOut })
+        .fromTo("#cDev", 3, { drawSVG: 0 }, { drawSVG: "102%" }, "-=1")
+        .to(".logo-stroke", 1, { autoAlpha: 1, ease: Linear.easeNone });
+      this.svgFilled = true;
     }
   },
   mounted() {
     this.animateText();
+    this.drawLogo();
   }
 };
 </script>
@@ -163,6 +258,10 @@ body {
   font-weight: 900;
   color: #353535;
   position: relative;
+}
+
+.text-hero {
+  margin-top: -3rem;
 }
 
 .text {
@@ -236,5 +335,34 @@ body {
 
 .text-turquoise {
   color: #c06c84;
+  transform: rotate(180deg);
+}
+
+.posts-container {
+  opacity: 0;
+}
+
+.draw {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  margin-left: -12rem;
+  width: 100%;
+  height: auto;
+}
+
+#cDev {
+  float: left;
+  overflow: visible;
+  margin-left: -2rem;
+}
+
+.logo-stroke {
+  stroke-width: 1.5px;
+  stroke: black;
+}
+
+.svg-filled {
+  fill: #c06c84;
+  stroke: none;
 }
 </style>
