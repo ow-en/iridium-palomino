833f93314ff47eb1db14e7a847207260efd691a1
ow-en <owen.email.sd@gmail.com>
2019-05-12 00:07:58 -0700

    fixed defaultView gsap error in build

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,8 +5,20 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+// const nodeExternals = require('webpack-node-externals');
+
 module.exports = function (api) {
-  api.loadSource(store => {
+  api.loadSource((store) => {
     // Use the Data store API here: https://gridsome.org/docs/data-store-api
-  })
-}
+  });
+
+  // api.chainWebpack((config, { isServer }) => {
+  //   if (isServer) {
+  //     config.externals(
+  //       nodeExternals({
+  //         whitelist: [/\.css$/, /\?vue&type=style/, /^gsap/],
+  //       }),
+  //     );
+  //   }
+  // });
+};
